86ae0dc1887accad6b87702a6c6d990698ccdcaf
ahmed ibrahem <arc.ami@live.com>
2025-07-04 17:19:49 +0200

    update

--- EraaSoftTask1/Program.cs
+++ EraaSoftTask1/Program.cs
@@ -4,31 +4,37 @@
     {
         static void Main(string[] args)
         {
-            // carpet washing firm
-            Console.WriteLine("welcome to our carpet wash firm");
+            // -------------------- carpet washing firm ----------------------------
+
+            Console.WriteLine("welcome to our carpet cleaning service");
             Console.WriteLine("Mr. Islam welcoming you ");
             // asking how many carpets
             Console.WriteLine("Please answer the next questions");
+
+            // small carpets
             Console.WriteLine("how many small carpets do you have?");
+            // waiting costumer input
             int smallCarpets = Convert.ToInt32(Console.ReadLine());
+
+            // large carpets
             Console.WriteLine("how many large carpets do you have?");
+            // waiting costumer input
             int largeCarpets = Convert.ToInt32(Console.ReadLine());
+
             // calculate the total cost
             int totalSmallCarpetsCost = smallCarpets * 25;
             int totalLargeCarpetsCost = largeCarpets * 35;
 
-
-            Console.WriteLine("================================================================");
-            Console.WriteLine("thank you for your answers");
+            // separator
             Console.WriteLine("================================================================");
             Console.WriteLine(" ");
 
-            Console.WriteLine("------------------------------------------------");
             // small carpets cost
             Console.WriteLine("the price for washing the small carpet is 25$");
             Console.WriteLine($"you have {smallCarpets} small carpet/s \n" +
                 $"with total cost {totalSmallCarpetsCost}");
             Console.WriteLine("------------------------------------------------");
+
             // large carpets cost
             Console.WriteLine("the price for washing the large carpet is 35$");
             Console.WriteLine($"you have {largeCarpets} large carpet/s \n" +
@@ -59,17 +65,14 @@
             Console.WriteLine("================================================================");
 
             // thank you message
-            Console.WriteLine("thank you for using our service, waiting for you in our place)");
-            Console.WriteLine("for more details, call 010 000 000 ");
+            Console.WriteLine("thank you for using our service");
+            Console.WriteLine("Estimates are valid for 30 days");
             Console.WriteLine("================================================================");
 
-            // end of the application
-            //-----------------------------------
-            // created by (ahmed m ibrahim) - back end course - eraasoft academy
             Console.ReadLine();
 
+            // ---------------- end of the application -------------------------------
 
-            // done
         }
     }
 }
